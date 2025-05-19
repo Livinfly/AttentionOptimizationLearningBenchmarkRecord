e1661a33b87dcfa820b1f3642c5a29696457ba63
Livinfly <luojie3m@gmail.com>
2025-05-19 17:48:32 +0800

    update

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
 # Benchmark_EXP
 
-默认以 LLaMa3 为例，测试各个模块的加速情况。
+默认以 CogVideoX-2b 为例，测试各个模块的加速情况。
+由于使用 RTX 4090 测试，不是 Hopper 架构，不能选择 fa3, sta 测试。
+测试对象为：sage2, sparge, sparge_fp8
+baseline: torch backend flash_sdp
+
+```bash
+python benchmark_end2end.py --compile --attention_type sdpa
+# Avg inference time: 88536.48 ms, runs=1
+python benchmark_end2end.py --compile --attention_type fa
+# Avg inference time: 88432.70 ms, runs=1
+python benchmark_end2end.py --compile --attention_type sage2
+# Avg inference time: 65574.58 ms, runs=1
+python benchmark_end2end.py --compile --attention_type sparge
+# Avg inference time: 67230.42 ms, runs=1
+python benchmark_end2end.py --compile --attention_type sparge_fp8
+# Avg inference time: 64075.88 ms, runs=1
+
+
+# sta 目前官方只有 Hopper 架构的实现，4090跑不了
+
+nsys profile -t cuda,nvtx -o profile_fa --stats=true --force-overwrite true python benchmark_end2end.py --comile --attnetion_type fa
+nsys profile -t cuda,nvtx -o profile_sage2 --stats=true --force-overwrite true python benchmark_end2end.py --comile --attnetion_type sage2
+nsys profile -t cuda,nvtx -o profile_sparge --stats=true --force-overwrite true python benchmark_end2end.py --comile --attnetion_type sparge
+nsys profile -t cuda,nvtx -o profile_sparge_fp8 --stats=true --force-overwrite true python benchmark_end2end.py --comile --attnetion_type sparge_fp8
+```
 
 ## FlashAttn (baseline)
 
 ```bash
+
 git clone https://github.com/Dao-AILab/flash-attention.git --recursive
 cd flash-attention
 git checkout b7d29fb3b79f0b78b1c369a52aaa6628dabfb0d7 # 2.7.2 release
-cd hopper
-python setup.py install
-```
 
-## FlashInfer
-
-```bash
-git clone https://github.com/flashinfer-ai/flashinfer.git --recursive
-cd flashinfer
-pip install -e . -v
+# cd hopper  # 支持 hopper 架构
+MAX_JOBS=8 python setup.py install # MAX_JOBS 不设小容易并行编译吃完内存被 kill 掉
 ```
 
 ## SageAttn
@@ -34,11 +52,5 @@ python setup.py install
 git clone https://github.com/thu-ml/SpargeAttn.git
 cd SpargeAttn
 python setup.py install
-```
-
-## STA (fastvideo)
-
-```bash
-pip install fastvideo
 ```
 
--- benchmark_end2end.py
+++ benchmark_end2end.py
@@ -7,29 +7,30 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str, default="THUDM/CogVideoX-2b", help='Model path')
 parser.add_argument('--compile', action='store_true', help='Compile the model')
-parser.add_argument('--attention_type', type=str, default='sdpa', choices=['sdpa', 'sage', 'fa3', 'fa3_fp8', 'sparge', 'flashinfer', 'sta'], help='Attention type')
+parser.add_argument('--attention_type', type=str, default='sdpa', choices=['sdpa', 'fa', 'sage2', 'sparge', 'sparge_fp8', 'flashinfer', 'sta'], help='Attention type')
 parser.add_argument('--warmup', type=int, default=2, help='Number of warmup runs')
-parser.add_argument('--runs', type=int, default=5, help='Number of measured runs')
+parser.add_argument('--runs', type=int, default=1, help='Number of measuremd runs')
 args = parser.parse_args()
 
-if args.attention_type == 'sage':
+if args.attention_type == 'sage2':
     from sageattention import sageattn
     F.scaled_dot_product_attention = sageattn
-elif args.attention_type == 'fa3':
-    from flash_attn.flash_attn_interface import flash_attn_func as fa3
-    F.scaled_dot_product_attention = fa3
-elif args.attention_type == 'fa3_fp8':
-    from flash_attn.flash_attn_interface import flash_attn_func_fp8 as fa3_fp8
-    F.scaled_dot_product_attention = fa3_fp8
+elif args.attention_type == 'fa':
+    torch.backends.cuda.enable_flash_sdp(True)
+    torch.backends.cuda.enable_mem_efficient_sdp(False)
+    torch.backends.cuda.enable_math_sdp(False)
 elif args.attention_type == 'sparge':
-    from sparge import sparge_attn
+    from spas_sage_attn import spas_sage_attn_meansim_cuda as sparge_attn
     F.scaled_dot_product_attention = sparge_attn
-elif args.attention_type == 'flashinfer':
-    from flashinfer import flashinfer_attn
-    F.scaled_dot_product_attention = flashinfer_attn
-elif args.attention_type == 'sta':
-    from sta import sta_attn
-    F.scaled_dot_product_attention = sta_attn
+elif args.attention_type == 'sparge_fp8':
+    from spas_sage_attn import spas_sage2_attn_meansim_cuda as sparge_attn
+    F.scaled_dot_product_attention = sparge_attn
+# elif args.attention_type == 'flashinfer':
+#     from wrapper import fi
+#     F.scaled_dot_product_attention = fi
+# elif args.attention_type == 'sta':
+#     from fastvideo import VideoGenerator
+#     F.scaled_dot_product_attention = sta_attn
 
 prompt = "A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance."
 
@@ -51,8 +52,8 @@ with torch.no_grad():
         _ = pipe(
             prompt=prompt,
             num_videos_per_prompt=1,
-            num_inference_steps=5,   # Use shorter steps/frames for warmup to save time
-            num_frames=8,
+            num_inference_steps=10,
+            num_frames=49,
             guidance_scale=6,
             generator=torch.Generator(device="cuda").manual_seed(42),
         )
@@ -82,4 +83,4 @@ with torch.no_grad():
 mean_time = sum(timings) / len(timings)
 print(f"Avg inference time: {mean_time:.2f} ms, runs={args.runs}")
 
-export_to_video(video, f"cogvideox-2b_{args.attention_type}.mp4", fps=8)
+# export_to_video(video, f"cogvideox-2b_{args.attention_type}.mp4", fps=8)
